301200082199ad604f81d8fdcf20c0b2e39150c2
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-08-02 14:24:56 +0100

    update

--- src/main/java/com/soft/app/dto/models/LaptopRequest.java
+++ src/main/java/com/soft/app/dto/models/LaptopRequest.java
@@ -1,10 +1,12 @@
 package com.soft.app.dto.models;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class LaptopRequest {
-	
+	@NotBlank
 	private String cpu;
 	private String hd;
 	private String ram;
--- src/main/java/com/soft/app/dto/models/TeacherRequest.java
+++ src/main/java/com/soft/app/dto/models/TeacherRequest.java
@@ -7,9 +7,12 @@ import org.modelmapper.ModelMapper;
 
 import com.soft.app.data.entity.Laptop;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
@@ -18,14 +21,17 @@ import lombok.Data;
 public class TeacherRequest {
 
 	@NotBlank
-	@Size(min = 4)
+	@Size(min = 4, max = 50)
 	private String matricule;
 
+	@Pattern(regexp = "^[a-zA-Z]{4,20}", message = "name must contain only letters")
 	private String firstName;
 	private String lastName;
 	private int score;
+	@Past
 	private LocalDate dateOfBirth;
 	
+	@Valid
 	private LaptopRequest laptop;
 	
 	private ClubRequest club;
